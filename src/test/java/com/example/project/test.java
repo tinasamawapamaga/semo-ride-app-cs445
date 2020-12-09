a5c7ea240661c4dbf88b53c263ef1b66c4a8b799
Tin Le <ttle1s@semo.edu>
2020-12-09 13:44:44 -0600

    test sample upload

--- src/test/java/com/example/project/test.java
+++ src/test/java/com/example/project/test.java
@@ -1,3 +1,11 @@
 package com.example.project;
+import static java.lang.Double.NaN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+public class test {
+    @Test
+    void function(){
 
-public class test {}
+    }
+}
